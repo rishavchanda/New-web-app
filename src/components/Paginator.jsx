13d8901060b5856ff006f30bb39bb7c266f9f15c
Rishav-Chanda <your_email_address@example.com>
2021-06-22 21:38:52 +0530

    added covid cases

--- src/components/Paginator.jsx
+++ src/components/Paginator.jsx
@@ -1,19 +1,36 @@
-import React, {useState,useEffect} from "react"
+import React, { useState, useEffect } from "react"
 
-const Pagination = ({showPerPage}) => {
-    const [counter, setCounter] = useState(1);
+const Pagination = ({ showPerPage, onPaginationChange, total }) => {
+  const [counter, setCounter] = useState(1);
+  useEffect(() => {
+    const value = showPerPage * counter;
+    onPaginationChange(value - showPerPage, value);
+    setNumberOfButtons(Math.ceil(total/6));        
+  }, [counter])
+  const [numberOfButtons, setNumberOfButtons] = useState();
+  const onButtonClick = (type) => {
+    if (type === "prev") {
+      if (counter === 1) {
+        setCounter(1)
+      } else {
+        setCounter(counter - 1)
+      }
+    } else if (type === "next") {
+      if (numberOfButtons === counter) {
+        setCounter(counter)
+      } else {
+        setCounter(counter + 1)
+      }
 
-    useEffect(()=>{
-      const value = showPerPage * counter;
-      
-    },[counter])
-    return(
-         <div className="d-flex justify-content-between">
-             <button className="btn btn-primary" onClick={() => setCounter(counter -1)}>Previous</button>
-             <button className="btn btn-primary" onClick={() => setCounter(counter +1)}>Next</button>
+    }
+  };
+  return (
+    <div className="d-flex justify-content-center">
+      <button className="btn buttons-background" onClick={() => onButtonClick("prev")}>&#8592;</button>
+      <button className="btn buttons-background " onClick={() => onButtonClick("next")}>&#8594;</button>
 
-         </div>
-    );
+    </div>
+  );
 };
 
 export default Pagination;
--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,11 +1,7 @@
 import React from "react"
 import { Redirect, Route, Switch } from 'react-router-dom';
 import Horizontal from "./Horizontal"
-import Vertical from "./Vertical"
 import Navigation from "../Navigation"
-import Toggler from "../Toggler"
-import FeedBack from "../FeedBack"
-import FeedBackForm from "./FeedBackForm";
 const Home = () => {
     return (
         <>
@@ -14,19 +10,10 @@ const Home = () => {
                     <div className="wrapper">
                         <div className="mt-4">
                             <Navigation />
-                            <Toggler />
-                            <FeedBack />
                         </div>
                     </div>
                     <div>
-                       
-                    <Switch>
-                      <Route exact path="/vertical" component={Vertical} />
-                      <Route exact path="/horizontal" component={Horizontal} />
-                    </Switch>
-                    <Switch>
-                    <Route exact path="/form" component={FeedBackForm} />
-                    </Switch>
+                   <Horizontal/>
                    </div>
                 </div>
             </div>
